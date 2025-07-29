37affcc955c0529348d6547de34706077d9e3c24
Anchal-2005-mohapatra <anchal08042005@gmail.com>
2025-07-29 16:56:17 +0530

    practice

---
+++ JavaScript/index.js
@@ -0,0 +1,112 @@
+let str = "hello"
+// console.log(str);
+// console.log(typeof str);
+
+let n = 4
+// console.log(n);
+// console.log(typeof n);
+
+let b = true
+// console.log(b);
+// console.log(typeof b);
+
+let u;
+// console.log(u);
+// console.log(typeof u);
+
+let nu = null
+// console.log(nu);
+// console.log(typeof nu);
+
+
+
+
+//Check the no is even or odd
+let a = 40
+// if(a%2==0)
+// {
+//     console.log(a, "the no is even");
+// }
+// else{
+//     console.log(a, "the no is odd");
+// }
+
+
+
+//using comparision operator to check wheather the no is even or odd
+let c = 34
+let d = 53
+// if(c>d)
+// {
+//     console.log(c , "is greater than", d );
+// }
+// else if(c==d)
+// {
+//     console.log(c, "is equals to ", d);
+// }
+// else{
+//     console.log(d, "is greater than to" , c)
+// }
+
+// concatination of a string and a number
+
+let no = 5
+let s = 'hii'
+// let re=no+s
+// console.log(re);
+// console.log(typeof re)
+
+
+//printing 1-10 using for and while loops
+
+for (let a = 1; a <= 10; a++) {
+    console.log(a);
+}
+
+
+let num = 1
+while (num <= 10) {
+    console.log(num);
+    num++
+}
+
+// do-while loop that print no from 5-1
+
+let i = 5
+// do{
+//     console.log(i);
+//     i--
+// }
+// while(i>0)
+
+
+
+//print each element of an array by using forof and for loop
+let arr = ["apple", "banana", "pineapple", "graphs", "strawary"]
+// for(let e of arr)
+// {
+//     console.log(e);
+// }
+
+// for(let i=0;i<arr.length;i++)
+// {
+//     console.log(arr[i]);
+// }
+
+//adding element at the beggining and end of an array
+let array = [1, 2, 4, 'apple', false, 5]
+// array.push(3,4,5,true,"fruits")
+// console.log(array)
+
+// array.unshift(4,5,6,'bingo')
+// console.log(array)
+
+//using splice method
+//  arr.splice(3,0,4,5,'solid')
+//  console.log(arr);
+
+//  arr.splice(3,2,'potato','spring',100)
+//  console.log(arr);
+
+//  arr.splice(3,2)
+//  console.log(arr);
