2e93409846a724ec0603462a96cf13ffa601ee0b
Anchal-2005-mohapatra <anchal08042005@gmail.com>
2025-08-23 16:06:48 +0530

    react

--- ReactJS/css-and-list/src/Components/InlineCss.jsx
+++ ReactJS/css-and-list/src/Components/InlineCss.jsx
@@ -2,7 +2,8 @@ import React from "react";
 const InlineCss=()=>
 {
     return(
-        <div><h1 style={{
+        <div>
+            <p id="lorem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod vel, vero libero voluptas molestias at assumenda eos commodi et voluptate recusandae tenetur aspernatur consequuntur amet error labore in quae.</p><h1 style={{
             color:"purple", 
             fontSize:"59px",
              textAlign:"center",
